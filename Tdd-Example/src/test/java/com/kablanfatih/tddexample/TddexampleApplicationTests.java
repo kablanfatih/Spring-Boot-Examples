2ab778579ea203dfeca509bf035930301ecfaacc
kablanfatih <kablanfatih34@gmail.com>
2020-02-16 11:56:59 +0300

    Initial commit

---
+++ Tdd-Example/src/test/java/com/kablanfatih/tddexample/TddexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kablanfatih.tddexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
