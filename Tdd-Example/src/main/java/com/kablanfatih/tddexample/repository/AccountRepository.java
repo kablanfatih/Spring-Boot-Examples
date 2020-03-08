d60492b80e3be7f1485452a6e255e2c54456f629
kablanfatih <kablanfatih34@gmail.com>
2020-03-08 21:23:56 +0300

    AccountRepository creating

--- Tdd-Example/src/main/java/com/kablanfatih/tddexample/repository/AccountRepository.java
+++ Tdd-Example/src/main/java/com/kablanfatih/tddexample/repository/AccountRepository.java
@@ -3,5 +3,6 @@ package com.kablanfatih.tddexample.repository;
 import com.kablanfatih.tddexample.entitiy.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AccountRepository extends JpaRepository<Account, String> {
 }
